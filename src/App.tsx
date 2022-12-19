a1e3f3bf997745e14dbdf187b73f953216edd78c
hugo wemer <wemer.eng@gmail.com>
2022-12-19 19:14:47 -0300

    Build: TaskList: initial structure and style.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import {Header} from './components/Header';
+import {TaskList} from './components/TaskList';
 import './global.css'
+
+import styles from './App.module.css';
 
 export function App() {
   return (
-    <Header />
+    <div className={styles.wrapper}>
+      <Header />
+      <TaskList/>
+    </div>
   )
 }
